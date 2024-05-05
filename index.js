95b01562df1a36c453db5d9330b44f0e49b3f556
Emily Chidester <erollinschidester@gmail.com>
2024-05-05 09:21:14 -0400

    Implement Error and Check Win Functionality
    Application will no longer allow you to write over another player's token
    and will now check for a winner after every round.
    
    Initial styling for the board on the webpage has also been implemented.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function Gameboard() {
 }
 
 function Square() {
-  let value = "-";
+  let value = "--";
 
   const updateValue = (player) => (value = player);
 
@@ -38,6 +38,8 @@ function GameController(
   playerTwoName = "Player 2"
 ) {
   const board = Gameboard();
+  const fullBoard = board.getBoard();
+  let gameActive = true;
 
   const players = [
     {
@@ -58,20 +60,100 @@ function GameController(
   const getActivePlayer = () => activePlayer;
 
   const playRound = (row, col) => {
-    const fullBoard = board.getBoard();
-    const activeSquare = fullBoard[row][col];
+    if (gameActive) {
+      const activeSquare = fullBoard[row][col];
 
-    activeSquare.updateValue(activePlayer.token);
-    switchPlayer();
-    board.printBoard();
-    console.log(activePlayer.name);
+      if (activeSquare.getValue() === "") {
+        activeSquare.updateValue(activePlayer.token);
+        if (!checkWin()) {
+          switchPlayer();
+          board.printBoard();
+          console.log(`It's ${activePlayer.name}'s turn.`);
+        }
+      } else {
+        board.printBoard();
+        console.log(`It's ${activePlayer.name}'s turn.`);
+      }
+    }
+  };
+
+  const checkLine = (cell1, cell2, cell3) => {
+    if (cell1 !== "" && cell1 === cell2 && cell2 === cell3) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const checkWin = () => {
+    // check down & across
+    for (let i = 0; i < 3; i++) {
+      if (
+        checkLine(
+          fullBoard[0][i].getValue(),
+          fullBoard[1][i].getValue(),
+          fullBoard[2][i].getValue()
+        ) ||
+        checkLine(
+          fullBoard[i][0].getValue(),
+          fullBoard[i][1].getValue(),
+          fullBoard[i][2].getValue()
+        )
+      ) {
+        // if either of these checkLines evaluate to true, there is a winner
+        console.log(`For loop...${i}`);
+        board.printBoard();
+        console.log(`${activePlayer.name} wins!`);
+        gameActive = false;
+        return true;
+      }
+    }
+
+    if (
+      checkLine(
+        fullBoard[0][0].getValue(),
+        fullBoard[1][1].getValue(),
+        fullBoard[2][2].getValue()
+      ) ||
+      checkLine(
+        fullBoard[0][2].getValue(),
+        fullBoard[1][1].getValue(),
+        fullBoard[2][0].getValue()
+      )
+    ) {
+      // if either of these checkLines evaluate to true, there is a winner
+      console.log(`Diagonals...`);
+      board.printBoard();
+      console.log(`${activePlayer.name} wins!`);
+      gameActive = false;
+      return true;
+    }
   };
 
   // Initialize game
   board.printBoard();
-  console.log(activePlayer.name);
+  console.log(`It's ${activePlayer.name}'s turn.`);
 
-  return { switchPlayer, getActivePlayer, playRound };
+  return { switchPlayer, getActivePlayer, playRound, checkWin, checkLine };
 }
 
-const game = GameController();
+const ScreenController = () => {
+  const board = Gameboard();
+  const fullBoard = board.getBoard();
+  const containerDiv = document.querySelector(".container");
+
+  const displayBoard = () =>
+    fullBoard.map((row) =>
+      row.map((square) => {
+        const newBtn = document.createElement("button");
+        newBtn.textContent = square.getValue();
+        containerDiv.appendChild(newBtn);
+      })
+    );
+
+  displayBoard();
+
+  return { displayBoard };
+};
+
+ScreenController();
